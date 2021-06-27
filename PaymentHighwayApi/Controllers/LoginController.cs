fcd54de7cf70cc6c6bc9795522e3fd87e29c499d
Anbin Anil <anbin.anil@teranet.ca>
2021-06-27 22:45:24 +0530

    test api

---
+++ PaymentHighwayApi/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentHighwayApi.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "a", "b" };
+        }
+    }
+}
